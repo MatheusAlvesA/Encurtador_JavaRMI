cbe19437ddffd3095b92444add2a0c01e4ad0002
Matheus Alves <matheuseejam@gmail.com>
2018-10-06 14:01:35 +0000

    Adicionando documentação

--- src/main/java/matheus/api/Endpoint.java
+++ src/main/java/matheus/api/Endpoint.java
@@ -20,21 +20,25 @@ import matheus.Encurtador.ServerFachada;
 import matheus.Encurtador.Server;
 import matheus.Encurtador.ServerException;
 /*
- * Esta classe representa a API
+ * Esta classe representa a API Restfu
  * */
-@Path("/")
+@Path("/") // Como esta é a unica classe, opera na raiz da API
 public class Endpoint {
 
-	private ServerFachada servidor;
-	Map<String, String> mensagensErro;
+	private ServerFachada servidor; // Guarda a instancia do servidor
+	private Map<String, String> mensagensErro; // Guarda as mensagens de erro possíveis
 	
+	/*
+	 * O construtor da classe inicializa o servidor e autopreenche as mensagens de erro
+	 * */
 	public Endpoint() {
 		try {
+			// Inicializando servidor através de sua interface
 			this.servidor = (ServerFachada) new Server("encurtador.matheusalves.com.br");
 		} catch (ServerException e) {
-			this.servidor = null;
+			this.servidor = null; // Em caso de falha o sistema saberá como reagir se o servidor for nulo
 		}
-		finally {
+		finally { // As mensagens de erro são preencidas independente de erros
 			this.mensagensErro = new HashMap<>();
 			this.mensagensErro.put("MANUTENCAO", "{\"status\": \"erro\", \"mensagem\": \"O servidor está passando por uma manutenção\"}");
 			this.mensagensErro.put("INVALID_REQUEST", "{\"status\": \"erro\", \"mensagem\": \"O corpo da requisição é inválido\"}");
@@ -42,7 +46,7 @@ public class Endpoint {
 		}
 	}
 	
-    @POST
+    @POST // Em caso de post deve encurtar uma URL
     @Produces(MediaType.APPLICATION_JSON)
     public Response encurtar(String corpo) {
     	if(this.servidor == null) 
@@ -64,13 +68,13 @@ public class Endpoint {
 			curta = this.servidor.encurtar(corpoJSON.getString("URL"));
 		}
     	catch (ServerException e) {
-    	  return Response.status(Response.Status.NOT_FOUND).entity("{\"status\": \"erro\", \"mensagem\": \""+e.getMessage()+"\"}").build();
+    	  return Response.status(Response.Status.BAD_REQUEST).entity("{\"status\": \"erro\", \"mensagem\": \""+e.getMessage()+"\"}").build();
     	}
-    	
+
     	return Response.status(Response.Status.OK).entity("{\"status\": \"sucesso\", \"curta\": \""+curta+"\"}").build();
     }
     
-    @GET
+    @GET // No caso do verbo get deve desencurtar uma URL
     @Produces(MediaType.APPLICATION_JSON)
     public Response desencurtar(@QueryParam("URL") String curta) {
     	if(this.servidor == null) 
@@ -84,9 +88,12 @@ public class Endpoint {
     		return Response.status(Response.Status.NOT_FOUND).entity("{\"status\": \"erro\", \"mensagem\": \""+e.getMessage()+"\"}").build();
     	}
    
-    	return Response.status(Response.Status.OK).entity("{\"status\": \"sucesso\", \"curta\": \""+longa+"\"}").build();
+    	return Response.status(Response.Status.OK).entity("{\"status\": \"sucesso\", \"longa\": \""+longa+"\"}").build();
     }
     
+    /*
+     * Os demais métodos HTTP não são suportados por esta API
+     * */
     @PUT
     @Produces(MediaType.APPLICATION_JSON)
     public Response PUT() {return this.notSuported();}
